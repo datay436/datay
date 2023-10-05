08717c6b0a520b38043728dd28e8ca1c55221470
datay436 <132848289+datay436@users.noreply.github.com>
2023-10-05 15:04:52 +0300

    lab 1

---
+++ abdul.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+string name;
+cout « "hello user, enter your name please: ";
+cin » name;
+cout « "hello, " « name « endl;
+return 0;
+}
+
